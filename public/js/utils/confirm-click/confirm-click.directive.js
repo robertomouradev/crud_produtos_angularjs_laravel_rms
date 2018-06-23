ac68e7e543f7d42e903d81e46d9e1b0271f1ed36
robertomouradev <40272961+robertomouradev@users.noreply.github.com>
2018-06-23 18:45:54 -0300

    directive confirm click

---
+++ public/js/utils/confirm-click/confirm-click.directive.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module("confirmClick").
+  directive('confirmClick', function(){
+    return {    
+        restrict: "A",
+        link: function (scope, element, attr) {
+            var msg = attr.confirmClick || "tem certeza?";
+            var clickAction = attr.confirmedClick;
+            element.bind('click',function (event) {
+                event.stopImmediatePropagation();
+                event.preventDefault();
+                if ( window.confirm(msg) ) {
+                    scope.$eval(clickAction)
+                } else {
+                    console.log("Cancelled")
+                }
+            });
+        }
+    }
+});
